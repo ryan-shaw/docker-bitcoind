506081dbf1ace5075a33354d07d72f56123f6bb4
Ryan Shaw <ryan.shaw@min.vc>
2018-01-25 18:57:18 +0000

    Update start script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
-set -e
+set -ex
 
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
 	set -- grlc_oneshot "$@"
-fi
-
-# allow the container to be started with `--user`
-if [ "$1" = 'grlc_oneshot' -a "$(id -u)" = '0' ]; then
-	chown -R garlicoin .
-	exec gosu garlicoin "$0" "$@"
 fi
 
 exec "$@"
